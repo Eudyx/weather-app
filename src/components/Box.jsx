f67a5b6e54f4b57077a1332123a2d927266355a1
Eudys Collado <eudys1.42@gmail.com>
2023-02-01 10:10:28 -0400

    Hook useCurrentLocation

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,14 +2,14 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import UpperBar from './UpperBar'
 import Weather from './Weather'
+import { useCurrentLocation } from '../hooks/useCurrentLocation';
 
 const Box = () => {
     const [ weather, setWeather ] = useState();
-    const [currentLocation, setCurrentLocation] = useState();
     const [selectedCity, setSelectedCity] = useState({center: []});
+    const [currentLocation, getLocation] = useCurrentLocation();
 
     // Tokens
-    const mapBox_token = 'pk.eyJ1IjoiZXVkeXgiLCJhIjoiY2xhemd5c2M4MHV4ajN2cnMzZGtxdmMxMyJ9.no-an8cBtT0Ai1FmyigQDw';
     const ow_token = '6e293401ee12e1cb0f99e77a2c253b44';
     let url = '';
 
@@ -21,46 +21,11 @@ const Box = () => {
     `https://api.openweathermap.org/data/2.5/weather?lat=${currentLocation.center[1]}&lon=${currentLocation.center[0]}&units=metric&appid=${ow_token}`
       : "" ;
 
-    // Requests to the api's
-    const getCurrentLocation = (lat, lon) => {
-      axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?
-        types=place%2Cpostcode%2Caddress&limit=1&access_token=${mapBox_token}`)
-          .then(res => setCurrentLocation(res.data.features[0]));
-    }
-
-    const getTheWeather = () => {
+      const getTheWeather = () => {
         axios.get(url)
             .then(res => {setWeather(res.data) 
               console.log(res.data)});
-    }
-
-    //GEOLOCATION
-
-    //geolocation options
-    const options = {
-      enableaHighAccuracy: false,
-      timeout: 5000,
-      maximumAge: 0
-    }
-
-    // In case of geolocation success
-    const success = (pos) => {
-      const crd = pos.coords;
-      getCurrentLocation(crd.latitude, crd.longitude);
-      
-      console.log(selectedCity);
-    }
-    
-    // Geolocation error
-    const error = (err) => {
-      console.log(`ERROR(${err.code}): ${err.message}`);
-    }
-
-    // Geolocation call
-    const getLocation = () => {
-      navigator.geolocation.getCurrentPosition(success, error, options);
-    }
+      }
 
     useEffect(() =>{
       getLocation();
--- src/hooks/useCurrentLocation.jsx
+++ src/hooks/useCurrentLocation.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import axios from "axios";
+
 export const useCurrentLocation = () => {
-    
-    
-    return [];
+    const [currentLocation, setCurrentLocation] = useState();
+    const mapBox_token = 'pk.eyJ1IjoiZXVkeXgiLCJhIjoiY2xhemd5c2M4MHV4ajN2cnMzZGtxdmMxMyJ9.no-an8cBtT0Ai1FmyigQDw';
+        // Requests to the api's
+        const getCurrentLocation = (lat, lon) => {
+            axios.get(
+              `https://api.mapbox.com/geocoding/v5/mapbox.places/${lon},${lat}.json?
+              types=place%2Cpostcode%2Caddress&limit=1&access_token=${mapBox_token}`)
+                .then(res => setCurrentLocation(res.data.features[0]));
+          }
+      
+          //GEOLOCATION
+      
+          //geolocation options
+          const options = {
+            enableaHighAccuracy: false,
+            timeout: 5000,
+            maximumAge: 0
+          }
+      
+          // In case of geolocation success
+          const success = (pos) => {
+            const crd = pos.coords;
+            getCurrentLocation(crd.latitude, crd.longitude);
+          }
+          
+          // Geolocation error
+          const error = (err) => {
+            console.log(`ERROR(${err.code}): ${err.message}`);
+          }
+      
+          // Geolocation call
+          const getLocation = () => {
+            navigator.geolocation.getCurrentPosition(success, error, options);
+          }
+
+    return [currentLocation, getLocation];
 }
